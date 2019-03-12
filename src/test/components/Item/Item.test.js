1732ccb338ea20edf717b6352bdf98de46da03cc
Luke Dyson <luke.dyson@flyinganvil.co.uk>
2019-03-12 19:48:05 +0000

    Updating tests and snapshots with new locations

--- src/test/components/Item/Item.test.js
+++ src/test/components/Item/Item.test.js
@@ -1,74 +1,77 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import { Item } from '../../../../src/components/Item/Item';
+import React from 'react'
+import { expect } from 'chai'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
+import { Item } from '../../../../src/components/Item/Item'
 
-const jestExpect = global.expect;
+const jestExpect = global.expect
 
-const defaultProps = { 
-  className: 'todo', 
+configure({adapter: new Adapter()})
+
+const defaultProps = {
+  className: 'todo',
   children: [
     <p key="1">hello 1</p>,
     <p key="2">hello 2</p>
-  ] 
-};
+  ]
+}
 
 describe('Item', () => {
   it('Should render without failing', () => {
-    const renderedItem = shallow(<Item {...defaultProps} />);
+    const renderedItem = shallow(<Item {...defaultProps} />)
 
-    jestExpect(toJson(renderedItem)).toMatchSnapshot();
-  });
+    jestExpect(toJson(renderedItem)).toMatchSnapshot()
+  })
 
   it('Should render with the class of "todoItem"', () => {
     const renderedItem = shallow(
       <Item {...defaultProps} />
-    );
+    )
 
-    expect(renderedItem.is('.todoItem')).to.equal(true);
-  });
+    expect(renderedItem.is('.todoItem')).to.equal(true)
+  })
 
   it('Should render with the class of "DEMO" when provided with a className as such', () => {
     const renderedItem = shallow(
       <Item {...defaultProps} className="DEMO"/>
-    );
+    )
 
-    expect(renderedItem.is('.todoItem.DEMO')).to.equal(true);
-  });
+    expect(renderedItem.is('.todoItem.DEMO')).to.equal(true)
+  })
 
   it('Should render as an "li" when no ElementType is provided', () => {
     const renderedItem = shallow(
       <Item {...defaultProps} />
-    );
+    )
 
-    expect(renderedItem.is('li')).to.equal(true);
-  });
+    expect(renderedItem.is('li')).to.equal(true)
+  })
 
   it('Should render as a "div" when the ElementType is "div"', () => {
     const renderedItem = shallow(
       <Item {...defaultProps} ElementType='div'/>
-    );
+    )
 
-    expect(renderedItem.is('div')).to.equal(true);
-  });
+    expect(renderedItem.is('div')).to.equal(true)
+  })
 
   it('Child div should render with a background colour when a valid colour is provided', () => {
-    const expectedBackgroundColor = '#456';
+    const expectedBackgroundColor = '#456'
     const renderedItem = shallow(
       <Item {...defaultProps} color={ expectedBackgroundColor }/>
-    );
+    )
 
-    expect(renderedItem.find('div').get(0).props.style.backgroundColor).to.equal(expectedBackgroundColor);
-  });
+    expect(renderedItem.find('div').get(0).props.style.backgroundColor).to.equal(expectedBackgroundColor)
+  })
 
   it('Should render provided children within its own wrapper elements', () => {
     const renderedItem = shallow(
       <Item {...defaultProps} />
-    );
+    )
 
-    expect(renderedItem.find('div').children().length).to.equal(2);
-    expect(renderedItem.find('div').children().get(0)).to.eql(defaultProps.children[0]);
-    expect(renderedItem.find('div').children().get(1)).to.eql(defaultProps.children[1]);
-  });
-});
+    expect(renderedItem.find('div').children().length).to.equal(2)
+    expect(renderedItem.find('div').children().get(0)).to.eql(defaultProps.children[0])
+    expect(renderedItem.find('div').children().get(1)).to.eql(defaultProps.children[1])
+  })
+})
--- src/test/components/Item/ItemCreator.test.js
+++ src/test/components/Item/ItemCreator.test.js
@@ -1,185 +1,188 @@
-import React from 'react';
-import chai, { expect } from 'chai';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import { ItemCreator } from '../../../../src/components/Item/ItemCreator';
-import Item from '../../../../src/components/Item/Item';
+import React from 'react'
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
+import { ItemCreator } from '../../../../src/components/Item/ItemCreator'
+import Item from '../../../../src/components/Item/Item'
 
-chai.use(sinonChai);
-const jestExpect = global.expect;
+chai.use(sinonChai)
+const jestExpect = global.expect
 
-const defaultProps = { 
+const defaultProps = {
   onAdd: sinon.stub()
-};
+}
+
+configure({adapter: new Adapter()})
 
 describe('ItemCreator', () => {
   it('Should render without failing', () => {
-    const renderedItem = shallow(<ItemCreator {...defaultProps} />);
+    const renderedItem = shallow(<ItemCreator {...defaultProps} />)
 
-    jestExpect(toJson(renderedItem)).toMatchSnapshot();
-  });
+    jestExpect(toJson(renderedItem)).toMatchSnapshot()
+  })
 
   it('Should render with the class of "itemCreator"', () => {
     const renderedItem = shallow(
       <ItemCreator {...defaultProps} />
-    );
+    )
 
-    expect(renderedItem.is('.itemCreator')).to.equal(true);
-  });
+    expect(renderedItem.is('.itemCreator')).to.equal(true)
+  })
 
   it('Should pass through that the required Item must be of type "div"', () => {
     const renderedItem = shallow(
       <ItemCreator {...defaultProps} />
-    );
+    )
 
-    expect(renderedItem.find(Item).props().ElementType).to.equal('div');
-  });
-  
+    expect(renderedItem.find(Item).props().ElementType).to.equal('div')
+  })
+
   describe('Should contain the following children', () => {
     it('"p"', () => {
       const renderedItem = shallow(
         <ItemCreator {...defaultProps} />
-      );
-  
-      expect(renderedItem.find('p')).not.to.be.undefined;
-      expect(renderedItem.find('p').props().children).to.equal('Add another item...');
-    });
+      )
+
+      expect(renderedItem.find('p')).not.to.be.undefined
+      expect(renderedItem.find('p').props().children).to.equal('Add another item...')
+    })
 
     it('"textarea"', () => {
       const renderedItem = shallow(
         <ItemCreator {...defaultProps} />
-      );
-  
-      expect(renderedItem.find('textarea')).not.to.be.undefined;
-      expect(renderedItem.find('textarea').props().placeholder).to.equal('What do you need to do?');
-    });
+      )
+
+      expect(renderedItem.find('textarea')).not.to.be.undefined
+      expect(renderedItem.find('textarea').props().placeholder).to.equal('What do you need to do?')
+    })
 
     it('"a"', () => {
       const renderedItem = shallow(
         <ItemCreator {...defaultProps} />
-      );
-  
-      expect(renderedItem.find('a')).not.to.be.undefined;
-      expect(renderedItem.find('a').props().className).to.equal('icon icono-plusCircle');
-    });
-  });
+      )
+
+      expect(renderedItem.find('a')).not.to.be.undefined
+      expect(renderedItem.find('a').props().className).to.equal('icon icono-plusCircle')
+    })
+  })
 
   describe('Should handle the following user initiated events', () => {
     afterEach(() => {
-      defaultProps.onAdd.reset();
-    });
+      defaultProps.onAdd.reset()
+    })
 
     it('Pressing enter on the textarea should call the "onAdd" method', () => {
       const renderedItem = shallow(
         <ItemCreator {...defaultProps} />
-      );
-      const itemCreatorTextArea = renderedItem.find('textarea');
+      )
+      const itemCreatorTextArea = renderedItem.find('textarea')
       const mockEvent = {
         key: 'Enter',
         preventDefault: sinon.stub()
-      };
-      const mockState = { inputValue: 'TEST ITEM' };
-    
-      renderedItem.setState(mockState);
+      }
+      const mockState = { inputValue: 'TEST ITEM' }
 
-      itemCreatorTextArea.props().onKeyPress(mockEvent);
-      
-      expect(mockEvent.preventDefault).to.have.been.called;
-      expect(defaultProps.onAdd).to.have.been.called.and.calledWith(mockState.inputValue);
-      expect(renderedItem.state('inputValue')).to.equal('');
-    });
+      renderedItem.setState(mockState)
+
+      itemCreatorTextArea.props().onKeyPress(mockEvent)
+
+      expect(mockEvent.preventDefault).to.have.been.called
+      expect(defaultProps.onAdd).to.have.been.called.and.calledWith(mockState.inputValue)
+      expect(renderedItem.state('inputValue')).to.equal('')
+    })
 
     describe('Pressing any other key on the textarea should not call the "onAdd" method', () => {
       it('"p" key', () => {
         const renderedItem = shallow(
           <ItemCreator {...defaultProps} />
-        );
-        const itemCreatorTextArea = renderedItem.find('textarea');
+        )
+        const itemCreatorTextArea = renderedItem.find('textarea')
         const mockEvent = {
           key: 'P',
           preventDefault: sinon.stub()
-        };
-        const mockState = { inputValue: 'TEST ITEM' };
-      
-        renderedItem.setState(mockState);
-  
-        itemCreatorTextArea.props().onKeyPress(mockEvent);
-        
-        expect(mockEvent.preventDefault).not.to.have.been.called;
-        expect(defaultProps.onAdd).not.to.have.been.called;
-        expect(renderedItem.state('inputValue')).to.equal(mockState.inputValue);
-      });
+        }
+        const mockState = { inputValue: 'TEST ITEM' }
+
+        renderedItem.setState(mockState)
+
+        itemCreatorTextArea.props().onKeyPress(mockEvent)
+
+        expect(mockEvent.preventDefault).not.to.have.been.called
+        expect(defaultProps.onAdd).not.to.have.been.called
+        expect(renderedItem.state('inputValue')).to.equal(mockState.inputValue)
+      })
 
       it('"Shift" key', () => {
         const renderedItem = shallow(
           <ItemCreator {...defaultProps} />
-        );
-        const itemCreatorTextArea = renderedItem.find('textarea');
+        )
+        const itemCreatorTextArea = renderedItem.find('textarea')
         const mockEvent = {
           key: 'Shift',
           preventDefault: sinon.stub()
-        };
-        const mockState = { inputValue: 'TEST ITEM' };
-      
-        renderedItem.setState(mockState);
-  
-        itemCreatorTextArea.props().onKeyPress(mockEvent);
-        
-        expect(mockEvent.preventDefault).not.to.have.been.called;
-        expect(defaultProps.onAdd).not.to.have.been.called;
-        expect(renderedItem.state('inputValue')).to.equal(mockState.inputValue);
-      });
+        }
+        const mockState = { inputValue: 'TEST ITEM' }
+
+        renderedItem.setState(mockState)
+
+        itemCreatorTextArea.props().onKeyPress(mockEvent)
+
+        expect(mockEvent.preventDefault).not.to.have.been.called
+        expect(defaultProps.onAdd).not.to.have.been.called
+        expect(renderedItem.state('inputValue')).to.equal(mockState.inputValue)
+      })
 
       it('"Up" key', () => {
         const renderedItem = shallow(
           <ItemCreator {...defaultProps} />
-        );
-        const itemCreatorTextArea = renderedItem.find('textarea');
+        )
+        const itemCreatorTextArea = renderedItem.find('textarea')
         const mockEvent = {
           key: 'Up',
           preventDefault: sinon.stub()
-        };
-        const mockState = { inputValue: 'TEST ITEM' };
-      
-        renderedItem.setState(mockState);
-  
-        itemCreatorTextArea.props().onKeyPress(mockEvent);
-        
-        expect(mockEvent.preventDefault).not.to.have.been.called;
-        expect(defaultProps.onAdd).not.to.have.been.called;
-        expect(renderedItem.state('inputValue')).to.equal(mockState.inputValue);
-      });
-    });
+        }
+        const mockState = { inputValue: 'TEST ITEM' }
+
+        renderedItem.setState(mockState)
+
+        itemCreatorTextArea.props().onKeyPress(mockEvent)
+
+        expect(mockEvent.preventDefault).not.to.have.been.called
+        expect(defaultProps.onAdd).not.to.have.been.called
+        expect(renderedItem.state('inputValue')).to.equal(mockState.inputValue)
+      })
+    })
 
     it('Clicking on the final anchor tag should call the "onAdd" method', () => {
       const renderedItem = shallow(
         <ItemCreator {...defaultProps} />
-      );
-      const itemCreatorAnchor = renderedItem.find('a');
-      const mockState = { inputValue: 'TEST ITEM' };
-    
-      renderedItem.setState(mockState);
+      )
+      const itemCreatorAnchor = renderedItem.find('a')
+      const mockState = { inputValue: 'TEST ITEM' }
 
-      itemCreatorAnchor.props().onClick();
-      
-      expect(defaultProps.onAdd).to.have.been.called.and.calledWith(mockState.inputValue);
-      expect(renderedItem.state('inputValue')).to.equal('');
-    });
+      renderedItem.setState(mockState)
+
+      itemCreatorAnchor.props().onClick()
+
+      expect(defaultProps.onAdd).to.have.been.called.and.calledWith(mockState.inputValue)
+      expect(renderedItem.state('inputValue')).to.equal('')
+    })
 
     it('Clicking on the final anchor tag should not call the "onAdd" method if there is no input value set', () => {
       const renderedItem = shallow(
         <ItemCreator {...defaultProps} />
-      );
-      const itemCreatorAnchor = renderedItem.find('a');
-      const mockState = { inputValue: '' };
-    
-      renderedItem.setState(mockState);
+      )
+      const itemCreatorAnchor = renderedItem.find('a')
+      const mockState = { inputValue: '' }
 
-      itemCreatorAnchor.props().onClick();
-      
-      expect(defaultProps.onAdd).not.to.have.been.called;
-    });
-  });
-});
+      renderedItem.setState(mockState)
+
+      itemCreatorAnchor.props().onClick()
+
+      expect(defaultProps.onAdd).not.to.have.been.called
+    })
+  })
+})
